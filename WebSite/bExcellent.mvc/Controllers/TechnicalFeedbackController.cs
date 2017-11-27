2d054c812265a5d250dbef29d6046873d3495387
LAPTOP-764PPRV3\Binesh <bineshcse@gmail.com>
2017-11-27 10:41:33 +0530

    technical

---
+++ WebSite/bExcellent.mvc/Controllers/TechnicalFeedbackController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bExcellent.mvc.Controllers
+{
+    public class TechnicalFeedbackController : Controller
+    {
+        //
+        // GET: /TechnicalFeedback/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SelfFeedback()
+        {
+            return View();
+        }
+        public ActionResult ManagerFeedback()
+        {
+            return View();
+        }
+
+    }
+}
